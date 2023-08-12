69366c298e70f345dbda40eac0768031a1db7b53
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-08-12 15:27:24 +0100

    added a new object which contains the details  about the uploaded video

--- routes/videos.js
+++ routes/videos.js
@@ -2,8 +2,7 @@ const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const videosData = require("../data/videos.json");
-const crypto = require("crypto")
-
+const crypto = require("crypto");
 
 // get all videos endpoint:
 
@@ -17,23 +16,38 @@ router.get("/:videosId", (request, response) => {
   const singleVideo = videosData.find((video) => {
     return video.id === request.params.videosId;
   });
-  response.json(singleVideo)
+  response.json(singleVideo);
 });
 
-router.post("/",(request,response)=>{
-    // need to generate a new id when posting
-    const newVideo = {
-        id: crypto.randomUUID(),
-        title: request.body.title,
-        description: request.body.description
+router.post("/", (request, response) => {
+  // need to generate a new id when posting
+  const newVideo = {
+    id: crypto.randomUUID(),
+    timestamp: 1626032763000,
+    views: 2000000,
+    likes: 100000,
+    title: request.body.title,
+    description: request.body.description,
+    // included the hardcoded static image which is located in the public folder
+    image: request.body.image, //using the url in the request body
+    comments: [
+      {
+        name: "Goku",
+        comment: "When is the next dragon ball movie",
+        timestamp: 1493645614,
+      },
+      {
+        name: "Gohan",
+        comment: "That was a cool video",
+        timestamp: 1893645614,
 
-    }
-    // push the new data to the json
-    videosData.push(newVideo)
-    // respond to client 
-    response.status(201).json(newVideo)
-})
-
-
+      },
+    ],
+  };
+  // push the new data to the json
+  videosData.push(newVideo);
+  // respond to client
+  response.status(201).json(newVideo);
+});
 
 module.exports = router;
