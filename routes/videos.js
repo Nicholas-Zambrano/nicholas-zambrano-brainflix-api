ac798d26556a47d8b565864014471ca87828070b
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-08-13 17:42:15 +0100

    getting the endpoint for the posteed comments and pushingthem to its associated video git status

--- routes/videos.js
+++ routes/videos.js
@@ -4,6 +4,43 @@ const router = express.Router();
 const videosData = require("../data/videos.json");
 const crypto = require("crypto");
 
+const randomNames = [
+  "nick",
+  "jack",
+  "alice",
+  "moises",
+  "pervis",
+  "piero",
+  "reascos",
+];
+
+const channelNames = [
+  "Lionel Messi",
+  "Cristiano Ronaldo",
+  "Neymar Jr",
+  "Kylian Mbappé",
+  "Robert Lewandowski",
+  "Mohamed Salah",
+  "Kevin De Bruyne",
+  "Raheem Sterling",
+];
+
+const randomTimestamps = [
+  1643654400000, 1673019200000, 1657296000000, 1675708800000, 1688352000000,
+  1625625600000, 1667328000000,
+];
+
+const randomViews = [
+  245345, 987652, 12345, 876543, 54321, 789012, 456789, 102938, 543210, 678901,
+];
+
+const randomLikes = [234, 567, 1234, 987, 345, 789, 456, 876, 654, 321];
+
+function randomizer(randomList) {
+  const randomIndex = Math.floor(Math.random() * randomList.length);
+  return randomList[randomIndex];
+}
+
 // get all videos endpoint:
 
 router.get("/", (request, response) => {
@@ -23,31 +60,42 @@ router.post("/", (request, response) => {
   // need to generate a new id when posting
   const newVideo = {
     id: crypto.randomUUID(),
-    timestamp: 1626032763000,
-    views: 2000000,
-    likes: 100000,
+    timestamp: randomizer(randomTimestamps),
+    views: randomizer(randomViews),
+    likes: randomizer(randomLikes),
     title: request.body.title,
     description: request.body.description,
     // included the hardcoded static image which is located in the public folder
     image: request.body.image, //using the url in the request body
-    comments: [
-      {
-        name: "Goku",
-        comment: "When is the next dragon ball movie",
-        timestamp: 1493645614,
-      },
-      {
-        name: "Gohan",
-        comment: "That was a cool video",
-        timestamp: 1893645614,
+    channel: randomizer(channelNames),
 
-      },
-    ],
+    comments: [],
   };
+
   // push the new data to the json
   videosData.push(newVideo);
   // respond to client
   response.status(201).json(newVideo);
+});
+
+router.post("/:videoId/comments", (request, response) => {
+  // accessing the value of the dyanmic parameter '/:videoId"
+  const videoId = request.params.videoId;
+  // returning that video with the same id as the value id in the url
+  const video = videosData.find((video) => {
+    return video.id === videoId;
+  });
+
+  // each property is assigned a value which will be displayed on the page when pushing the data
+  // the comment porperty is assigned with the value from comment field which was inputted from user in the client side
+  const newComment = {
+    // calling the randomizer function to select a random name
+    name: randomizer(randomNames),
+    comment: request.body.comment,
+    timestamp: Date.now(),
+  };
+  // then we push the new comments to that associated/selected video comments array
+  video.comments.push(newComment);
 });
 
 module.exports = router;
